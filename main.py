7f9e19f8fd93eaccabc07fe2fab45f4352a223ea
appfromape <appfromape@gmail.com>
2021-03-27 13:22:45 +0800

    finished

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify, redirect, make_response
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = "todolist"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    
+# db.create_all()
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    posts = Todo.query.all()
+    return render_template("index.html", all_posts=posts)
+
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    if request.method == 'POST':
+        print(request.form.get('item'))
+        new_item = Todo(
+            content = request.form['item']
+        )
+        db.session.add(new_item)
+        db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
+@app.route('/delete', methods=["GET", "POST"])
+def delete():
+    if request.method == 'POST':
+        a = request.form['keyword']
+        print(a)
+        todo = Todo.query.filter_by(content=a).first()
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
     
     $("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500,function(){
+        
+        var search_word = $(this).text();
+        var qurl="http://127.0.0.1:5000/delete";
+        $.ajax({
+            type: "POST",
+            cache: false,
+            data:{keyword:search_word},
+            url: qurl,
+            dataType: "json"
+        })
+    
     $(this).remove();
     });
     event.stopPropagation();
@@ -14,17 +25,15 @@ $(document).ready(function(){
     
     $("input[type='text']").keypress(function(event){
     if(event.which === 13){
-    
+    event.submit();
     var todoText = $(this).val();
     $(this).val("");
-    
-    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " +todoText + "</li>")
     }
     });
-    
+
     $(".fa-plus").click(function(){
     $("input[type='text']").fadeToggle();
     });
-    
     
     });
